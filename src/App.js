92dd44fcf16a276411876acde2fb2e90925c756d
alexelash <alash@azavea.com>
2021-02-18 19:39:39 +0400

    Style app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import GitHubLogo from "./GitHub-Mark-Light-120px-plus.png";
-import AzaveaLogo from "./azavea_white_solo.svg";
-import "./App.css";
-import loam from "loam";
-import FilePicker from "./FilePicker";
-import GeoTiffInfo from "./GeoTiffInfo";
-import About from "./About";
+import GitHubLogo from './GitHub-Mark-Light-120px-plus.png';
+import AzaveaLogo from './azavea_white_solo.svg';
+import './App.css';
+import loam from 'loam';
+import FilePicker from './FilePicker';
+import GeoTiffInfo from './GeoTiffInfo';
+import About from './About';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from "react-router-dom";
-import Modal from "react-modal";
-import { useEffect, useState } from "react";
+} from 'react-router-dom';
+import Modal from 'react-modal';
+import { useEffect, useState } from 'react';
 
 const EPSG4326 =
   'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]';
@@ -24,14 +24,14 @@ function App() {
   const [gtifWidth, setGtifWidth] = useState(null);
   const [gtifHeight, setGtifHeight] = useState(null);
   const [gtifBands, setGtifBands] = useState(null);
-  const [gtifWkt, setGtifWkt] = useState("");
+  const [gtifWkt, setGtifWkt] = useState('');
   const [gtifTransform, setGtifTransform] = useState(null);
   const [cornersGeo, setCornersGeo] = useState(null);
   const [cornersLngLat, setCornersLngLat] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    loam.initialize("/").then(setLoamLoaded(true));
+    loam.initialize('/').then(setLoamLoaded(true));
   }, []);
 
   useEffect(() => {
@@ -52,7 +52,7 @@ function App() {
       ];
     });
 
-    loam.reproject(gtifWkt, EPSG4326, cornersGeo).then((cornersLngLat) => {
+    loam.reproject(gtifWkt, EPSG4326, cornersGeo).then(cornersLngLat => {
       setCornersGeo({
         ll: cornersGeo[0],
         lr: cornersGeo[1],
@@ -72,14 +72,14 @@ function App() {
     selectedFile &&
       loam
         .open(selectedFile)
-        .then((ds) => {
-          ds.width().then((width) => setGtifWidth(width));
-          ds.height().then((height) => setGtifHeight(height));
-          ds.count().then((count) => setGtifBands(count));
-          ds.wkt().then((wkt) => setGtifWkt(wkt));
-          ds.transform().then((geoTransform) => setGtifTransform(geoTransform));
+        .then(ds => {
+          ds.width().then(width => setGtifWidth(width));
+          ds.height().then(height => setGtifHeight(height));
+          ds.count().then(count => setGtifBands(count));
+          ds.wkt().then(wkt => setGtifWkt(wkt));
+          ds.transform().then(geoTransform => setGtifTransform(geoTransform));
         })
-        .catch((err) => setErrorMessage(err.message));
+        .catch(err => setErrorMessage(err.message));
   }, [selectedFile]);
 
   return (
@@ -87,60 +87,84 @@ function App() {
       <Modal
         isOpen={isModalOpen}
         onRequestClose={() => setIsModalOpen(false)}
-        className="modal"
+        className='modal'
         style={{
           content: {
-            backgroundColor: "#27323d",
-            color: "#fefefe",
-            maxWidth: "50%",
+            backgroundColor: '#27323d',
+            color: '#fefefe',
+            width: '90%',
+            maxWidth: '500px',
           },
         }}
       >
-        <div className="modal-header">
-          <h2>About</h2>
+        <div className='modal-header'>
+          <h1 className='modal-title'>About</h1>
           <button
-            className="modal-close-button"
+            className='modal-close-button'
             onClick={() => setIsModalOpen(false)}
           >
-            ✖
+            <i
+              className='info-content__close-button-icon fal fa-times-circle fa-lg'
+              alt='Close'
+            ></i>
           </button>
         </div>
-        <div className="modal-body">
+        <div className='modal-body'>
           <About />
         </div>
       </Modal>
       <Router>
-        <header className="header">
-          <div>
-            <img src={AzaveaLogo} alt="Azavea logo" height="32px" /> | Loam: Run
-            GDAL in the browser
-          </div>
-          <div>
+        <header className='header'>
+          <h1 className='header__title-container'>
+            <img
+              src={AzaveaLogo}
+              alt='Azavea'
+              height='73'
+              width='300'
+              className='header__logo'
+            />
+            <span className='header__divider' aria-hidden='true'>
+              |
+            </span>
+            <span className='header__title'>Loam: Run GDAL in the browser</span>
+          </h1>
+          <div className='header__menu'>
             <button
-              className="about-button"
+              className='header__about-button'
               onClick={() => setIsModalOpen(true)}
             >
               About
             </button>
-            <a href="https://github.com/azavea/loam">
-              <img src={GitHubLogo} width="32px" alt="GitHub logo" />
+            <a
+              href='https://github.com/azavea/loam'
+              className='header__github-link'
+              title='Go to project’s GitHub'
+            >
+              <img
+                src={GitHubLogo}
+                className='header__github-icon'
+                width='120'
+                height='120'
+                alt='GitHub'
+                aria-hidden='true'
+              />
             </a>
           </div>
         </header>
-        <main className="code">
-          <div className="content">
+        <main className='main'>
+          <div className='content'>
             <Switch>
-              <Route exact path="/">
+              <Route exact path='/'>
                 {!loamLoaded && (
-                  <img src="" className="spinner" alt="loading-spinner" />
+                  <img src='' className='spinner' alt='loading-spinner' />
                 )}
                 {loamLoaded && (
                   <FilePicker
-                    onFileSelect={(file) => {
+                    onFileSelect={file => {
                       setGtifWidth(null);
                       setGtifHeight(null);
                       setGtifBands(null);
-                      setGtifWkt("");
+                      setGtifWkt('');
                       setGtifTransform(null);
                       setCornersGeo(null);
                       setCornersLngLat(null);
@@ -150,7 +174,7 @@ function App() {
                   />
                 )}
               </Route>
-              <Route path="/geotiff">
+              <Route path='/geotiff'>
                 {selectedFile && (
                   <GeoTiffInfo
                     name={selectedFile.name}
@@ -163,7 +187,7 @@ function App() {
                     errorMessage={errorMessage}
                   />
                 )}
-                {!selectedFile && <Redirect to="/" />}
+                {!selectedFile && <Redirect to='/' />}
               </Route>
             </Switch>
           </div>
--- src/FilePicker.js
+++ src/FilePicker.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
-import Dropzone from "react-dropzone";
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Dropzone from 'react-dropzone';
 
 function FilePicker({ onFileSelect }) {
   const history = useHistory();
   const [isDragging, setIsDragging] = useState(false);
 
-  const handleFileChange = (files) => {
+  const handleFileChange = files => {
     onFileSelect(files[0]);
-    history.push("/geotiff");
+    history.push('/geotiff');
   };
 
   return (
@@ -20,21 +20,47 @@ function FilePicker({ onFileSelect }) {
       onDragLeave={() => setIsDragging(false)}
     >
       {({ getRootProps, getInputProps }) => (
-        <div {...getRootProps()}>
-          <div style={{ border: isDragging ? "2px #f1b310 solid" : "none" }}>
-            {!isDragging ? (
-              <>
-                <p>
-                  Drag & drop or select a GeoTIFF from your file system.
-                  <br />
-                  Information about the file will be displayed.
-                </p>
-              </>
-            ) : (
-              <p>Drop your GeoTIFF here</p>
-            )}
-            <button>Browse...</button>
-            <input {...getInputProps()} />
+        <div className='drag-container__focus' {...getRootProps()}>
+          <div
+            className={
+              isDragging
+                ? 'drag-container drag-container--dragging'
+                : 'drag-container'
+            }
+          >
+            <div className='drag-container__text-container'>
+              {!isDragging ? (
+                <>
+                  <h2 className='drag-container__title'>
+                    geotiffinfo(filename)
+                  </h2>
+                  <div>
+                    <i
+                      className='drag-container__dragging-icon fas fa-sparkles fa-lg'
+                      aria-hidden='true'
+                    ></i>
+                  </div>
+                  <p className='drag-container__desc'>
+                    Drag-and-drop or select a GeoTIFF from your file system.
+                    Information about the file will be displayed.
+                  </p>
+                  <button className='button button--primary drag-container__button'>
+                    Browse...
+                  </button>
+                </>
+              ) : (
+                <>
+                  <div>
+                    <i
+                      className='drag-container__dragging-icon fas fa-plus fa-lg'
+                      aria-hidden='true'
+                    ></i>
+                  </div>
+                  <p className='drag-container__desc'>Drop your GeoTIFF here</p>
+                </>
+              )}
+              <input {...getInputProps()} />
+            </div>
           </div>
         </div>
       )}
